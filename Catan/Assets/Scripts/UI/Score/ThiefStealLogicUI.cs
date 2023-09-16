eb43a57d9695adc957b24d0a26e0be08d25e16ed
Ali Fuat <87227396+AliFuat98@users.noreply.github.com>
2023-09-16 17:24:28 +0300

    steal source from user

--- Catan/Assets/Scripts/UI/Score/ThiefStealLogicUI.cs
+++ Catan/Assets/Scripts/UI/Score/ThiefStealLogicUI.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class ThiefStealLogicUI : MonoBehaviour {
-  private ulong playerScoreID;
 
   private void Awake() {
     var button = GetComponent<Button>();
@@ -15,7 +14,6 @@ public class ThiefStealLogicUI : MonoBehaviour {
 
   private void Start() {
     gameObject.gameObject.SetActive(false);
-    playerScoreID = transform.GetComponentInParent<PlayerScoreUI>().GetPlayerScoreClientId();
 
     CatanGameManager.Instance.OnThiefSteal += CatanGameManager_OnThiefSteal;
     CatanGameManager.Instance.OnThiefPlaced += CatanGameManager_OnThiefPlaced;
@@ -27,6 +25,11 @@ public class ThiefStealLogicUI : MonoBehaviour {
   }
 
   private void CatanGameManager_OnThiefPlaced(object sender, CatanGameManager.OnThiefPlacedEventArgs e) {
+    var playerScoreUI = GetComponentInParent<PlayerScoreUI>();
+    if (playerScoreUI == null) {
+      return;
+    }
+    var playerScoreID = playerScoreUI.GetPlayerScoreClientId();
     if (e.ownerClientIDList.Contains(playerScoreID)) {
       gameObject.SetActive(true);
     } else {
@@ -39,5 +42,85 @@ public class ThiefStealLogicUI : MonoBehaviour {
   }
 
   private void StealSource() {
+    var playerScoreUI = GetComponentInParent<PlayerScoreUI>();
+    if (playerScoreUI == null) {
+      return;
+    }
+    var playerScoreID = playerScoreUI.GetPlayerScoreClientId();
+
+    var catanInstance = CatanGameManager.Instance;
+
+    var localPlayerData = catanInstance.GetLocalPlayerData();
+    var localPlayerIndex = catanInstance.GetPlayerDataIndexFromClientID(localPlayerData.clientId);
+
+    var victomPlayerData = catanInstance.GetPlayerDataFromClientId(playerScoreID);
+    var victomPlayerDataIndex = catanInstance.GetPlayerDataIndexFromClientID(playerScoreID);
+
+    if (victomPlayerData.balyaCount +
+      victomPlayerData.koyunCount +
+      victomPlayerData.kerpitCOunt +
+      victomPlayerData.mountainCoun +
+      victomPlayerData.odunCount == 0) {
+      // there is no source to steal
+      return;
+    }
+
+    var randomSourceIndex = Random.Range(0, 5);
+    for (var i = 0; i < 5; i++) {
+      randomSourceIndex++;
+      randomSourceIndex %= 5;
+      switch (randomSourceIndex) {
+        case 0: // bread balya
+
+          if (victomPlayerData.balyaCount > 0) {
+            victomPlayerData.balyaCount--;
+            localPlayerData.balyaCount++;
+            i += 5;
+          }
+          break;
+
+        case 1: // koyun
+
+          if (victomPlayerData.koyunCount > 0) {
+            victomPlayerData.koyunCount--;
+            localPlayerData.koyunCount++;
+            i += 5;
+          }
+          break;
+
+        case 2: // kerpit
+
+          if (victomPlayerData.kerpitCOunt > 0) {
+            victomPlayerData.kerpitCOunt--;
+            localPlayerData.kerpitCOunt++;
+            i += 5;
+          }
+          break;
+
+        case 3: // odun
+
+          if (victomPlayerData.odunCount > 0) {
+            victomPlayerData.odunCount--;
+            localPlayerData.odunCount++;
+            i += 5;
+          }
+          break;
+
+        case 4: // mountain
+
+          if (victomPlayerData.mountainCoun > 0) {
+            victomPlayerData.mountainCoun--;
+            localPlayerData.mountainCoun++;
+            i += 5;
+          }
+          break;
+
+        default:
+          break;
+      }
+    }
+
+    catanInstance.SetPlayerDataFromIndex(victomPlayerDataIndex, victomPlayerData);
+    catanInstance.SetPlayerDataFromIndex(localPlayerIndex, localPlayerData);
   }
 }
