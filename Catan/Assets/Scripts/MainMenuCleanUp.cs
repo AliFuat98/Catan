733d4e751a3c09a0dc70ebbb010205250ca6c27b
Ali Fuat <87227396+AliFuat98@users.noreply.github.com>
2023-09-19 11:58:31 +0300

    loading scene

--- Catan/Assets/Scripts/MainMenuCleanUp.cs
+++ Catan/Assets/Scripts/MainMenuCleanUp.cs
@@ -1,18 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class MainMenuCleanUp : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class MainMenuCleanUp : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+  private void Awake() {
+    if (NetworkManager.Singleton != null) {
+      Destroy(NetworkManager.Singleton.gameObject);
     }
+  }
 }
